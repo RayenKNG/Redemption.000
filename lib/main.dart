648ba825279fe3021f02142a38e23fbc153205ad
Rayen467 <133085728+Rayen467@users.noreply.github.com>
2026-01-09 05:08:10 +0700

    telah menambahkan import 'firebase_options.dart';pada file main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'screens/home_screen.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
