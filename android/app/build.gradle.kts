d42a7892bb5f51dca1427785b5ddcfef23636bda
Rayen467 <133085728+Rayen467@users.noreply.github.com>
2026-01-09 11:52:05 +0700

    menambahkan dependencies android/app/build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
+    
 }
 
 android {
@@ -45,4 +45,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+
+dependencies {
+    
 }
