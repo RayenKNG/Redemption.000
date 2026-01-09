56166ba425421874cc2c797757058a19d38c078f
Rayen467 <133085728+Rayen467@users.noreply.github.com>
2026-01-09 20:34:44 +0700

    membuat StatefulWidget LoginScreen pada file login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,18 @@
 import 'package:flutter/material.dart';
 import '../services/auth_service.dart';
 import 'dart:ui';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // Kita return Scaffold dulu biar layar ada isinya (putih polos + teks)
+    return const Scaffold(body: Center(child: Text("Setup Login Screen")));
+  }
+}
