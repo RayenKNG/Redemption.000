37bbc987bd980f55984c363bcc2d950745135230
Rayen467 <133085728+Rayen467@users.noreply.github.com>
2026-01-09 20:55:51 +0700

    menTambahkan Scaffold dasar. pada file login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,7 +12,28 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    // Kita return Scaffold dulu biar layar ada isinya (putih polos + teks)
-    return const Scaffold(body: Center(child: Text("Setup Login Screen")));
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          // Langkah 1: Gradasi warna Hijau SavePlate
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFFE8F5E9), // Hijau sangat muda (atas kiri)
+              Color(0xFF2ECC71), // Hijau Emerald (bawah kanan)
+            ],
+          ),
+        ),
+        // Kita siapin Stack buat numpuk elemen kaca nanti
+        child: Stack(
+          children: [
+            // Kosong dulu, nanti kita isi dekorasi & form login
+          ],
+        ),
+      ),
+    );
   }
 }
